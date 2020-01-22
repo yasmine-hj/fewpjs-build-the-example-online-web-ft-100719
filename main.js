63e5961c8f757c378d3a0f3419faf9c7f3e1bfac
yasmine-hj <yasminehajaji95@gmail.com>
2020-01-22 12:27:53 +0000

    Done.

--- main.js
+++ main.js
@@ -3,8 +3,34 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+let shapes = {
+  "♡": "♥",
+  "♥": "♡"
+};
 
+const hearts = document.getElementsByClassName("like-glyph")
 
+function likeFunction(event) {
+  const heart = event.target;
+  mimicServerCall("url")
+  
+  .then(message => {
+      heart.innerText = shapes[heart.innerText];
+      heart.classList.contains("activated-heart") ? heart.classList.remove("activated-heart") : heart.classList.add("activated-heart"); 
+    })
+
+  .catch(error => {
+    document.getElementById("modal").className = "";
+    document.getElementById("modal-message").innerText = error.message;
+    setTimeout(function(){
+      document.getElementById("modal").className = "hidden";
+    }, 3000);
+  })
+}
+
+for (const heart of hearts) {
+  heart.addEventListener("click", likeFunction);
+}
 
 
 //------------------------------------------------------------------------------
